e8c3b0be9a81ede56973a3db72b3e23569f35268
ThomasAlbertCreeRahn <trahn826@student.fuhsd.org>
2023-02-13 14:16:19 -0800

    Initial Commit

---
+++ RecordCamera/RecordCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordCameraApp.swift
+//  RecordCamera
+//
+//  Created by Thomas Rahn on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct RecordCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
